114f50f5cb7d33cfce2d739bc68b6f4d06a9a69b
unknown <zhougb@liuhf-top.cuctv.com>
2017-03-27 18:36:17 +0800

    init project

---
+++ shardingdb-protocol/src/test/java/com/zy/dbproxy/test/MainTest.java
@@ -0,0 +1,14 @@
+package com.zy.dbproxy.test;
+
+import org.junit.Test;
+
+/**
+ * Created by zhougb on 2017/3/27.
+ */
+public class MainTest {
+
+    @Test
+    public void t(){
+        System.out.println("t");
+    }
+}
